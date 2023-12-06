4c38680dd6beeed8eac35e62c97ac33409e1862d
jorgerf05 <jorgefco_rf@hotmail.com>
2023-12-06 03:08:24 -0800

    buttons integration

--- botite.py
+++ botite.py
@@ -17,7 +17,13 @@ bot.
 import logging
 from typing import Dict
 
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import (
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -25,128 +31,129 @@ from telegram.ext import (
     ConversationHandler,
     MessageHandler,
     filters,
+    CallbackQueryHandler,
 )
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
 logging.getLogger("httpx").setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
 
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
 
-reply_keyboard = [
-    ["Preguntar", "Escolares"],
-    ["Carreras", "Localizar"],
-    ["Terminar"],
-]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-
-def facts_to_str(user_data: Dict[str, str]) -> str:
-    """Helper function for formatting the gathered user info."""
-    facts = [f"{key} - {value}" for key, value in user_data.items()]
-    return "\n".join(facts).join(["\n", "\n"])
+# Main states
+MAIN_MENU, SUB_MENU, END_ROUTES = range(3)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Start the conversation and ask user for input."""
-    await update.message.reply_text(
-        "Hola! Soy el ITEbot. Fui creado con el propocito de ayudarte a responder tus preguntas, ayudarte a conocer tu escuela y tu carrera asi como otras funciones extras. "
-        "Dime en que te puedo ayudar o elige una opcion de nuestro menu?",
-        reply_markup=markup,
+    user = update.message.from_user
+    keyboard = [
+        [
+            InlineKeyboardButton(" Sistemas 🖥️", callback_data="sistemas"),
+            InlineKeyboardButton(" Innovación Agrícola  🌾", callback_data="agricola"),
+        ],
+        [
+            InlineKeyboardButton(
+                " Electromecánica ⚙️", callback_data="electromecanica"
+            ),
+            InlineKeyboardButton(" Gestión Empresarial 📈", callback_data="gestion"),
+        ],
+        [
+            InlineKeyboardButton(" Industrial 🏭", callback_data="industrial"),
+            InlineKeyboardButton(" Mecatrónica 🤖", callback_data="mecatronica"),
+        ],
+        [
+            InlineKeyboardButton(" Administración 📊", callback_data="admin"),
+            InlineKeyboardButton(" Electrónica 📡", callback_data="electronica"),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Elige una Ingeniería", reply_markup=reply_markup)
+
+    return MAIN_MENU
+
+
+async def show_submenu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+
+    keyboard = [
+        [
+            InlineKeyboardButton("Noticias 📰", callback_data="noticias"),
+            InlineKeyboardButton("Escolares 🎒", callback_data="escolares"),
+        ],
+        [
+            InlineKeyboardButton("Carreras 🎓", callback_data="carreras"),
+            InlineKeyboardButton("Mapas 🗺️", callback_data="mapas"),
+        ],
+        [
+            InlineKeyboardButton("Horario ⏰", callback_data="horario"),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Editar el mensaje original con el submenu
+    await query.edit_message_text(
+        text="Selecciona una opción de tu interés", reply_markup=reply_markup
     )
 
-    return CHOOSING
+    # Cambiar a la etapa SUB_MENU
+    return SUB_MENU
 
 
-async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """La eleccion fue : """
-    text = update.message.text
-    context.user_data["choice"] = text
-    await update.message.reply_text(f"Your {text.lower()}? Yes, I would love to hear about that!")
+async def handle_submenu_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+    chosen_option = query.data
 
-    return TYPING_REPLY
+    match chosen_option:
+        case "noticias":
+            print("notifififi")
+            pass
+        case "carreras":
+            pass
+        case "mapas":
+            pass
+        case "escolares":
+            pass
+        case "horario":
+            pass
+    # Editar el mensaje original con la opción seleccionada del submenu
+    await query.edit_message_text(text=f"Selected option in submenu: {chosen_option}")
 
-
-async def custom_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Que tipo de informacion necesitas."""
-    await update.message.reply_text(
-        'Alright, please send me the category first'
-    )
-
-    return TYPING_CHOICE
-
-
-async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Store info provided by user and ask for the next category."""
-    user_data = context.user_data
-    text = update.message.text
-    category = user_data["choice"]
-    user_data[category] = text
-    del user_data["choice"]
-
-    await update.message.reply_text(
-        "Neat! Just so you know, this is what you already told me:"
-        f"{facts_to_str(user_data)}You can tell me more, or change your opinion"
-        " on something.",
-        reply_markup=markup,
-    )
-
-    return CHOOSING
+    # Cambiar a la etapa END_ROUTES
+    return END_ROUTES
 
 
-async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Display the gathered info and end the conversation."""
-    user_data = context.user_data
-    if "choice" in user_data:
-        del user_data["choice"]
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
 
-    await update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
-        reply_markup=ReplyKeyboardRemove(),
-    )
+    await query.edit_message_text(text="See you next time!")
 
-    user_data.clear()
     return ConversationHandler.END
 
 
 def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token("6332760312:AAE7kTnvUPO1mdqtfZR22v3uVG5ueLbUXG0").build()
+    application = (
+        Application.builder()
+        .token("6332760312:AAE7kTnvUPO1mdqtfZR22v3uVG5ueLbUXG0")
+        .build()
+    )
 
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("iniciar", start)],
         states={
-            CHOOSING: [
-                MessageHandler(
-                    filters.Regex("^(Preguntar|Escolares|Number of siblings)$"), regular_choice
-                ),
-                MessageHandler(filters.Regex("^Algo en lo que te pueda asistir...$"), custom_choice),
+            MAIN_MENU: [CallbackQueryHandler(show_submenu)],
+            SUB_MENU: [
+                CallbackQueryHandler(handle_submenu_choice),
             ],
-            TYPING_CHOICE: [
-                MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Terminar$")), regular_choice
-                )
-            ],
-            TYPING_REPLY: [
-                MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Terminar$")),
-                    received_information,
-                )
-            ],
+            END_ROUTES: [CommandHandler("adios", start)],
         },
-        fallbacks=[MessageHandler(filters.Regex("^Terminar$"), done)],
+        fallbacks=[CommandHandler("start", start)],
     )
 
     application.add_handler(conv_handler)
-
-    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
